85df27a0b510473dc6ff32bbe3e81abcda79a843
Shubham Choudhary <shubhamc@mindfiresolutions.com>
2026-04-06 16:46:05 +0530

    seaprate the react app from the backend

--- DeployDemoTest/UnitTest1.cs
+++ DeployDemoTest/UnitTest1.cs
@@ -8,11 +8,6 @@ namespace DeployDemoTest
 {
     public class UnitTest1
     {
-        //[Fact]
-        //public void Test1()
-        //{
-
-        //}
         [Fact]
         public async Task GetAll_ReturnsAllRecords()
         {
--- front-deploydemo/src/common-components/protectroute.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-export const ProtectedRoute = () => {
-  const isAuthenticated = !!localStorage.getItem('token'); // Check your login logic
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
-};
